ec9fc8329acd75ccb327261fc0012f5910a43520
beyond-godlike <dia_the_ghost@mail.ru>
2019-09-11 14:09:52 +0300

    added tests for HeroPickerActivity

---
+++ app/src/androidTest/java/com/unava/dia/dotapedia2reborn/ui/heroConstructor/picker/HeroPickerActivityTest.kt
@@ -0,0 +1,22 @@
+package com.unava.dia.dotapedia2reborn.ui.heroConstructor.picker
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class HeroPickerActivityTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun onCreate() {
+    }
+}
