eb5962a29cbe9fab46afe4ef9adab9811585ea46
beyond-godlike <dia_the_ghost@mail.ru>
2019-09-28 17:59:26 +0300

    no message

--- app/src/main/java/com/unava/dia/dotapedia2reborn/ui/updates/update/OneUpdateActivity.kt
+++ app/src/main/java/com/unava/dia/dotapedia2reborn/ui/updates/update/OneUpdateActivity.kt
@@ -1,10 +1,12 @@
 package com.unava.dia.dotapedia2reborn.ui.updates.update
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import com.unava.dia.dotapedia2reborn.R
 import dagger.android.AndroidInjection
+import kotlinx.android.synthetic.main.activity_one_update.*
 import javax.inject.Inject
 
 class OneUpdateActivity : AppCompatActivity() {
@@ -14,12 +16,20 @@ class OneUpdateActivity : AppCompatActivity() {
     @Inject
     lateinit var viewModel: OneUpdateViewModel
 
+    private var articleUrl: String? = null
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_one_update)
         AndroidInjection.inject(this)
         this.bindViewModel()
         init()
+
+        webWiew.settings.javaScriptEnabled = true
+        if(savedInstanceState != null) {
+            articleUrl = savedInstanceState.getString("URL_TO_FULL_ARTICLE")
+        }
     }
 
     private fun init() {
